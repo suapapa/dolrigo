fdf346cbef9a1e2b03de81ed25a1951216b586e8
Homin Lee <ff4500@gmail.com>
2023-06-01 07:11:57 +0900

    add game type

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module gihub.com/suapapa/dolrigo
 
 go 1.20
 
-require github.com/gin-gonic/gin v1.9.0
+require (
+	github.com/gin-gonic/gin v1.9.0
+	google.golang.org/api v0.125.0
+)
 
 require (
 	cloud.google.com/go/compute v1.19.3 // indirect
@@ -34,7 +37,6 @@ require (
 	golang.org/x/oauth2 v0.8.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/api v0.125.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	google.golang.org/grpc v1.55.0 // indirect
--- main.go
+++ main.go
@@ -1,33 +1,55 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/idtoken"
 )
 
+type ClientChan chan string
+
 var (
-	survyeID = "1P_Myt5-7aRncmZ2AJwC0UwhkmEAEqxCdJpUxhjmp_SM"
+	subPath     string
+	templateDir string
+	survyeID    = "1P_Myt5-7aRncmZ2AJwC0UwhkmEAEqxCdJpUxhjmp_SM"
 )
 
 func main() {
+	flag.StringVar(&subPath, "p", "/", "subpath")
+	flag.StringVar(&templateDir, "t", "template", "template dir")
+	flag.Parse()
+
+	if !strings.HasSuffix(subPath, "/") {
+		subPath += "/"
+	}
 
 	r := gin.Default()
-	r.LoadHTMLGlob("/template/*.html")
-	r.GET("/dolrigo/ping", func(c *gin.Context) {
+
+	r.LoadHTMLGlob(path.Join(templateDir, "*.html"))
+	r.GET(subPath+"ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong from dolrigo",
 		})
 	})
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.String(404, "Not Found")
-	// })
-	r.GET("/dolrigo", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
+	r.GET(subPath+":gid/join", func(c *gin.Context) {
+		gid := c.Param("gid")
+		type JoinData struct {
+			GID      string
+			ClientID string
+			SubPath  string
+		}
+		c.HTML(http.StatusOK, "join.html", &JoinData{
+			GID:      gid,
+			SubPath:  subPath,
+			ClientID: "340420184719-6s9f9t49o3nme4lu1f52vjnh3tmerptb.apps.googleusercontent.com",
+		})
 	})
-	r.POST("/dolrigo/login", func(c *gin.Context) {
+	r.POST(subPath+":gid/sse", func(c *gin.Context) {
 		p, err := idtoken.Validate(c.Request.Context(), c.PostForm("credential"), "")
 		if err != nil {
 			c.String(500, "Internal Server Error 2")
@@ -37,30 +59,27 @@ func main() {
 		for k, v := range p.Claims {
 			log.Println(k, v)
 		}
+		user := &Candidate{
+			Name:    p.Claims["name"].(string),
+			EMail:   p.Claims["email"].(string),
+			Picture: p.Claims["picture"].(string),
+		}
+
+		gid := c.Param("gid")
+		if _, ok := Games[gid]; !ok {
+			Games[gid] = NewGame()
+		}
+		game := Games[gid]
+		game.AddCandidate(user)
+	})
+	r.GET(subPath+":gid/spin", func(c *gin.Context) {
+		gid := c.Param("gid")
+		if _, ok := Games[gid]; !ok {
+			Games[gid] = NewGame()
+		}
+		game := Games[gid]
+		c.JSON(200, game)
 	})
 
 	r.Run(":8080") // listen and serve on
-
-	// // Get the Google Survey results.
-	// resp, err := http.Get(fmt.Sprintf("https://forms.googleapis.com/v1/forms/%s/responses", survyeID))
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// defer resp.Body.Close()
-	// io.Copy(os.Stdout, resp.Body)
-
-	// Decode the JSON response.
-	// results := make([]map[string]interface{}, 0)
-	// err = json.NewDecoder(resp.Body).Decode(&results)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// // Do something with the results.
-	// for _, result := range results {
-	// 	fmt.Println(result)
-	// }
 }
